69e2efae8aa0cec58866136ec9f7e57875d0382f
Salma Elshaelshahawy <salma71@gmail.com>
2018-06-18 13:18:12 -0400

    render the index component

--- src/index.js
+++ src/index.js
@@ -2,14 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route } from 'react-router-dom'
 
-import App from './components/app';
 import reducers from './reducers';
+import PostsIndex from './components/posts_index'
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
+
+
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+    {/* if I went to that path --> then show that component, this two props are very important */}
+      <div>
+        <Route path="/" component={PostsIndex}/>
+      </div>
+    </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
