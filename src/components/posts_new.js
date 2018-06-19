101bfe8b7ae6d431c9f1e66e9b999f5b238f2f62
Salma Elshaelshahawy <salma71@gmail.com>
2018-06-19 10:58:19 -0400

    submit the post to the backend API

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,10 +1,15 @@
 import React, {Component} from 'react';
 import {Field, reduxForm} from 'redux-form';
+import {Link} from 'react-router-dom';
+import {connect} from 'react-redux';
+import {createPost} from '../actions';
 
 class PostsNew extends Component {
     renderField(field) {
+        const {meta: {touched, error}} = field;
+        const className = `form-group ${touched && error ? 'has-danger' : ''}`
         return(
-            <div className="form-group">
+            <div className={className}>
                 <label>{field.label}</label>
                 <input 
                 className="form-control"
@@ -14,12 +19,18 @@ class PostsNew extends Component {
                 // onBlur={field.input.onBlur}
                     {...field.input}
                 />
-                {field.meta.touched ? field.meta.error : ''}
+                    <div className="text-help"> 
+                    {touched ? error : ''}
+                    </div>
             </div>
         )
     }
     onSubmit(values) {
         // console.log(values)
+        // this.props.history.push('/') // we have to refresh to see the newly created post
+        this.props.createPost(values, () => {
+            this.props.history.push('/')
+        })
     }
     render() {
         const {handleSubmit} = this.props;
@@ -43,6 +54,7 @@ will be visible on screen to our users */}
                     component={this.renderField} //takes a function
                 />
                 <button type="submit" className="btn btn-primary">Submit</button>
+                <Link to="/" className="btn btn-danger">Cancel</Link>
             </form>
         )
     }
@@ -65,6 +77,8 @@ function validate(values){
 export default reduxForm({
     validate,
     form: 'PostsNewForm' // this string should be unique
-})(PostsNew); 
+})(
+    connect(null, {createPost})(PostsNew)
+);
 // by doing that, we gave redux form the ability to communicate directly
 // from this component to the reducer that we wired up 
